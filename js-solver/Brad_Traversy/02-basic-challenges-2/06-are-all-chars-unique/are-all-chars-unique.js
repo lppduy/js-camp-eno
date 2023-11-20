f38b680b99879e2ca6528cab15d3959f12f76e91
lppduy <lephamphuongduy@gmail.com>
2023-11-20 15:57:09 +0700

    update brad coding practice

--- js-solver/Brad_Traversy/02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ js-solver/Brad_Traversy/02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,11 +1,20 @@
-function areAllCharactersUnique(str) {
-  const charCount = {};
+function areAllCharactersUnique1(str) {
+  const charCount = [];
   for (let i = 0; i < str.length; i++) {
-    const currentChar = str[i];
-    if (charCount[currentChar]) return false;
-    charCount[currentChar] = true;
+    const char = str[i];
+    if (charCount[char]) return false;
+    charCount[char] = true;
+  }
+  return true;
+}
+function areAllCharactersUnique2(str) {
+  const charSet = new Set();
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charSet.has(char)) return false;
+    charSet.add(char);
   }
   return true;
 }
 
-module.exports = areAllCharactersUnique;
+module.exports = areAllCharactersUnique2;
--- js-solver/Brad_Traversy/02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ js-solver/Brad_Traversy/02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,22 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  const charCount = {};
+  for (let char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+  for (let char of str) {
+    if (charCount[char] === 1) return char;
+  }
+  return null;
+}
+function findFirstNonRepeatingCharacter2(str) {
+  const charMap = new Map();
+  for (const char of str) {
+    charMap.set(char, (charMap.get(char) || 0) + 1);
+  }
+  for (const char of str) {
+    if (charMap.get(char) === 1) return char;
+  }
+  return null;
+}
 
-module.exports = findFirstNonRepeatingCharacter;
+module.exports = findFirstNonRepeatingCharacter2;
