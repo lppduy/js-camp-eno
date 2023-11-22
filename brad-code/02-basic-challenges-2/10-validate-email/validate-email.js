e04cc6b9ded49cbee0dfa4b234e88bc83c76e936
lppduy <lephamphuongduy@gmail.com>
2023-11-22 22:14:54 +0700

    brad code - finished chap 2

--- brad-code/02-basic-challenges-2/10-validate-email/validate-email.js
+++ brad-code/02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,14 @@
-function validateEmail() {}
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+function validateEmail2(email) {
+  if (email.indexOf('@') === -1) return false;
+  const [localPart, domain] = email.split('@');
+  if (localPart.length === 0 || domain.length < 3) return false;
+  const domainExtension = domain.split('.');
+  if (domainExtension.length < 2 || domainExtension[1] < 2) return false;
+  return true;
+}
 
-module.exports = validateEmail;
+module.exports = validateEmail2;
